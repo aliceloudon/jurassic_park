55cfe9e40fc6755c3ab5b1c77167a295c6e9f44e
Alice Loudon <alice_loudon@icloud.com>
2017-04-04 17:36:10 +0100

    successfully can get all the dinosaurs with an average offspring count of more than 2 and pass test

--- park.js
+++ park.js
@@ -3,15 +3,27 @@ var Park = function() {
 }
 
 Park.prototype = {
+
   addDinosaur: function(dinosaur) {
     this.enclosure.push(dinosaur)
   },
+
   removeAllDinosaursOfType: function(dinosaurType) {
     for (var i = this.enclosure.length - 1; i >= 0; i--) {
       if (this.enclosure[i].type === dinosaurType) {
         this.enclosure.splice(i, 1)
       }
     }
+  },
+
+  getDinosaursWithOffspringGreaterThan2: function() {
+    var found = []
+    for (var i = this.enclosure.length - 1; i >= 0; i--) {
+      if (this.enclosure[i].offspring > 2) {
+        found.push(this.enclosure[i])
+      }
+    }
+    return found
   }
 
 }
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -8,9 +8,10 @@ describe ('Park', function() {
 
   beforeEach(function() {
     park = new Park()
-    dinosaur = new Dinosaur('Tyrannosaurus', 2)
-    dinosaur2 = new Dinosaur('Tyrannosaurus', 2)
+    dinosaur = new Dinosaur('Tyrannosaurus', 1)
+    dinosaur2 = new Dinosaur('Tyrannosaurus', 1)
     dinosaur3 = new Dinosaur('Velociraptor', 5)
+    dinosaur4 = new Dinosaur('Triceratops', 7)
   })
 
   it ('should have an empty enclosure at start', function() {
@@ -28,6 +29,13 @@ describe ('Park', function() {
     park.addDinosaur(dinosaur3)
     park.removeAllDinosaursOfType('Tyrannosaurus')
     assert.strictEqual(1, park.enclosure.length)
+  })
+
+  it ('can get all the dinosaurs with an average offspring count of more than 2', function() {
+    park.addDinosaur(dinosaur)
+    park.addDinosaur(dinosaur3)
+    park.addDinosaur(dinosaur4)
+    assert.strictEqual(2, park.getDinosaursWithOffspringGreaterThan2().length)
   })
 
 })
